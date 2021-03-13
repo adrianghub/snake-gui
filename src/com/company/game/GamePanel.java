4241b7c387a29592ae8a77b21dd4bc7c95d19826
Adrian <adrian_zinko@op.pl>
2021-03-13 11:27:57 +0100

    Init: Prepare methods for game panel

--- src/com/company/game/GamePanel.java
+++ src/com/company/game/GamePanel.java
@@ -1,8 +1,12 @@
 package com.company.game;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
@@ -10,8 +14,43 @@ public class GamePanel extends JPanel implements ActionListener {
 
     }
 
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+
+    public void checkFeed() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
     }
 }
